97f5fb1ee64786156409cec20c82592c2ff248da
unknown <leonovfx.91@gmail.com>
2021-06-13 17:23:34 +0300

    -proxy

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const getPizzasData =(filters)=>{
-  return  axios(`/pizzas?${filters.category ? `category=${filters.category}`:''}&_sort=${filters.sortBy.type}`)
+  return  axios(`"http://localhost:3001/pizzas?${filters.category ? `category=${filters.category}`:''}&_sort=${filters.sortBy.type}`)
     .then(({data})=> data)
 }
 
